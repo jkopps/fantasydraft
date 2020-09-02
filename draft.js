6ec8acb03f6d4d87b3da7568391abb651d96f063
Jeff Koppernolle <jeffrey.koppernolle@gmail.com>
2020-09-02 12:23:01 -0400

    Added initial event handler

--- draft.js
+++ draft.js
@@ -1,6 +1,19 @@
+function displayTeamDepthChart(t) {
+    dc = document.getElementById('depthchart');
+    dc.innerHTML = t;
+}
+
+function selectPlayer(p){
+    t = p.team;
+    displayTeamDepthChart(t);
+}
+
 function addPlayer(p){
     players = document.getElementById('players');
     row = players.insertRow(-1);
+    row.addEventListener("click",
+			 function() { selectPlayer(p); }
+			)
     row.insertCell(-1).innerHTML = p.rank;
     row.insertCell(-1).innerHTML = p.pos;
     row.insertCell(-1).innerHTML = p.team;
